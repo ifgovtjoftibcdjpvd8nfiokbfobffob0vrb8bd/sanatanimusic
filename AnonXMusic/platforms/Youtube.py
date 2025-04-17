cefd95364244f0cf8e916e462d0db988d6eb4b2c
ANURAG YADAV <139148328+mrcutex1@users.noreply.github.com>
2025-04-18 00:26:03 +0530

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -15,10 +15,9 @@ from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from youtubesearchpython.__future__ import VideosSearch, CustomSearch
 
-from AnonXMusic import LOGGER
-from AnonXMusic.utils.database import is_on_off
+from config import YT_API_KEY, YTPROXY_URL as YTPROXY 
 from AnonXMusic.utils.formatters import time_to_seconds
-from config import YT_API_KEY, YTPROXY_URL as YTPROXY
+from AnonXMusic import LOGGER
 
 
 async def shell_cmd(cmd):
